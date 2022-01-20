251209e7c65fbd36464ef051da6e97e4f6db3686
chandni singh <chandnisingh8055@gmail.com>
2022-01-20 11:49:33 +0530

    main template setup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Bulls and Cows game!");
+        System.out.println("This is a logical mind games" +
+                "\n Here B means one of your guessing number is inside the hidden number and in the right place"
+                + "\n Here C means one of your guessing number is inside the hidden number and in the Wrong place ");
 
     }
 }

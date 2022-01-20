4a825135935cf9d989595186cc25d5a98eb5e792
chandni singh <chandnisingh8055@gmail.com>
2022-01-20 12:19:16 +0530

    while loop for guessing hidden numbers

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,54 @@ public class Main {
 
         Random random = new Random();
         int intRandom = random.nextInt(99);
+
+        while(intRandom<11){
+            intRandom++;
+        }
+
+        String s = String.valueOf(intRandom);
+        char charRandom1 = s.charAt(0);
+        char charRandom2 = s.charAt(1);
+
+        while(true)
+        {
+            if(charRandom1==charRandom2)
+            {
+                charRandom1++;
+            }
+            System.out.println("Enter your guess");
+            String strRandom = scanner.nextLine();
+
+            int input = Integer.parseInt(strRandom);
+            if(input<10)
+            {
+                System.out.println("you entered one level number, please enter two level number");
+                continue;
+            }
+            char charInput = strRandom.charAt(0);
+            char charInput2 = strRandom.charAt(1);
+
+            if(input==intRandom)
+            {
+                System.out.println("Congratulations");
+                return;
+            }
+            else if(charRandom1==charInput || charRandom2==charInput2)
+            {
+                System.out.println("1B,0C");
+            }
+            else if(charRandom1==charInput2 || charRandom2==charInput)
+            {
+                System.out.println("0B,2C");
+            }
+            else if(charRandom1==charInput2 || charRandom2==charInput)
+            {
+                System.out.println("0B,1C");
+            }
+            else
+            {
+                System.out.println("0B,0C");
+            }
+        }
     }
 }
